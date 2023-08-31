2158b0af157d50e587e5310cbcfca6ae54274dc0
spacecat1945 <kokonoserin414@gmail.com>
2023-08-31 17:40:10 +0900

    easylogin

---
+++ src/main/java/jp/co/internous/easylogin/EssyloginApplication.java
@@ -0,0 +1,13 @@
+package jp.co.internous.easylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssyloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssyloginApplication.class, args);
+	}
+
+}
